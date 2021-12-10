594636df33c72c210e68e1ce689183fd2af113be
mighidc <95907363+mighidc@users.noreply.github.com>
2021-12-10 10:42:02 +0000

    10dicembre

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import sys
+import math
+import numpy as np
+
+r = 12+5j
+print(type(r))
+print(type(r.real))
+
+a=4.0872
+print(type(a))
+
+#c = not(3 and 6)
+#print(c)
+
+f=math.sin(np.pi/6)
+print(f)
+m=math.cos(30)
+print(m)
+
+a2= 'hola' + ' amigos'
+print(a2)
+
+name = 'Mighi'
+mystring="hola %s" %name
+print(mystring)
+
+mystring2="hola %s! Come va? %s" %(name, "Bene, grazie!! Tu?")
+print(mystring2)
+
+g="How\'s the day today?"
+print(g)
