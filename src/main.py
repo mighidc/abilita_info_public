64eb24679a1403969fc1a2c394f7a71b91cb9f58
mighidc <95907363+mighidc@users.noreply.github.com>
2021-12-10 10:59:20 +0000

    prove ulteriori

--- src/main.py
+++ src/main.py
@@ -20,6 +20,12 @@ print(m)
 a2= 'hola' + ' amigos'
 print(a2)
 
+a3= 'hola\tamigos'
+print(a3)
+
+n=a3.replace("a", "aa", 2)
+print(n)
+
 name = 'Mighi'
 mystring="hola %s" %name
 print(mystring)
@@ -27,5 +33,10 @@ print(mystring)
 mystring2="hola %s! Come va? %s" %(name, "Bene, grazie!! Tu?")
 print(mystring2)
 
-g="How\'s the day today?"
+g="What\'s the day today?"
 print(g)
+
+o = "oggi è %s %d %s" %("venerdì", 10, "dicembre")
+print(o)
+
+
